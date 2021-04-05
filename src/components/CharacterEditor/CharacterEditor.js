9ace5e124ba4803518b5c5b90431adc58b6d2c74
Katrin Fritz <katrinfritz@web.de>
2021-04-05 13:21:16 +0200

    Style desktop and mobile view

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveBar} />
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -38,43 +39,45 @@ function App() {
             controls below. What sort of adventure will you embark on?{' '}
           </p>
         </header>
-        <div className={styles.controlColumn}>
-          <ControlPane
-            title="Bodies"
-            options={bodyOptions}
-            currentOption={body}
-            handleSelectOption={setBody}
-          />
-          <ControlPane
-            title="Heads"
-            options={headOptions}
-            currentOption={head}
-            handleSelectOption={setHead}
-          />
-          <ControlPane
-            title="Faces"
-            options={faceOptions}
-            currentOption={face}
-            handleSelectOption={setFace}
-          />
-          <ControlPane
-            title="Accessories"
-            options={accessoryOptions}
-            currentOption={accessory}
-            handleSelectOption={setAccessory}
-          />
-          <ControlPane
-            title="Skin Color"
-            options={skinColorOptions}
-            currentOption={skinColor}
-            handleSelectOption={setSkinColor}
-          />
-          <ControlPane
-            title="Clothing Color"
-            options={clothesColorOptions}
-            currentOption={clothesColor}
-            handleSelectOption={setClothesColor}
-          />
+        <div style={{width: '100%', position: 'relative'}}>
+          <div className={styles.controlColumn}>
+            <ControlPane
+              title="Bodies"
+              options={bodyOptions}
+              currentOption={body}
+              handleSelectOption={setBody}
+            />
+            <ControlPane
+              title="Heads"
+              options={headOptions}
+              currentOption={head}
+              handleSelectOption={setHead}
+            />
+            <ControlPane
+              title="Faces"
+              options={faceOptions}
+              currentOption={face}
+              handleSelectOption={setFace}
+            />
+            <ControlPane
+              title="Accessories"
+              options={accessoryOptions}
+              currentOption={accessory}
+              handleSelectOption={setAccessory}
+            />
+            <ControlPane
+              title="Skin Color"
+              options={skinColorOptions}
+              currentOption={skinColor}
+              handleSelectOption={setSkinColor}
+            />
+            <ControlPane
+              title="Clothing Color"
+              options={clothesColorOptions}
+              currentOption={clothesColor}
+              handleSelectOption={setClothesColor}
+            />
+          </div>
         </div>
       </MaxWidthWrapper>
 
